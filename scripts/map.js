f3c9be1abe63d1c4b684a53891d450af5aa781a7
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2023-11-08 13:09:39 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -210,7 +210,6 @@ $(window).on('load', function() {
         }
       }
     }
-
       // Update table every time the map is moved/zoomed or point layers are toggled
       map.on('moveend', updateTable);
       map.on('layeradd', updateTable);
@@ -909,7 +908,7 @@ $(window).on('load', function() {
    */
   function changeAttribution() {
     var attributionHTML = $('.leaflet-control-attribution')[0].innerHTML;
-    var credit = 'View <a href="' + 'https://www.castle-development.com/' + '" target="_blank">data</a>';
+    var credit = 'View <a href="' + 'https://www.cred-eg.com/' + '" target="_blank">data</a>';
     var name = getSetting('_authorName');
     var url = getSetting('_authorURL');
 
